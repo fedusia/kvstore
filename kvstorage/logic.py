1dbc58352f7d7f74a9ab73d06c172acf2955c579
Dmitry S. Fedorov <fedusia@yandex-team.ru>
2021-07-06 13:50:18 +0300

    Create simple stucture for project

---
+++ kvstorage/logic.py
@@ -0,0 +1,13 @@
+class Storage:
+    """
+    Dependency Injection pattern
+    """
+
+    def __init__(self, engine):
+        self.engine = engine
+
+    def get(self, key):
+        return self.engine.get(key)
+
+    def set(self, key, value):
+        return self.engine.set(key, value)
